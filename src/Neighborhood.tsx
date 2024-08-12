41faf2d4b554cb58f15bd80866e3ed75aa651b09
Nick Ooms <oomsni@gmail.com>
2024-08-12 11:33:04 +0200

    alert location

--- src/Neighborhood.tsx
+++ src/Neighborhood.tsx
@@ -4,29 +4,28 @@ import { OrbitControls } from '@react-three/drei';
 const URL = 'https://geo.api.vlaanderen.be/geolocation/v4/location';
 
 const useCurrentLocation = () => {
+  const [location, setLocation] = useState(null);
+
   const position = useCurrentPosition();
   const latitude = position?.latitude;
   const longitude = position?.longitude;
-  const [location, setLocation] = useState(null);
+
   useEffect(() => {
     fetch(`${URL}?latlon=${latitude},${longitude}`)
       .then((response) => response.json())
       .then(setLocation)
       .catch(console.error);
   }, [latitude, longitude]);
+
   return location;
 };
 
 const useCurrentPosition = () => {
   const [position, setPosition] = useState<GeolocationCoordinates | null>(null);
 
-  console.log('useCurrentPosition', position);
-
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      (position) => {
-        setPosition(position.coords);
-      },
+      (position) => setPosition(position.coords),
       console.error,
       { enableHighAccuracy: true },
     );
@@ -37,13 +36,11 @@ const useCurrentPosition = () => {
 
 export const Neighborhood = () => {
   const location = useCurrentLocation();
-  console.log(location);
-  /* const position = useCurrentPosition();
-  console.log(position); */
+
   return (
     <>
       <ambientLight />
-      <mesh>
+      <mesh onClick={() => alert(JSON.stringify(location, null, 2))}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="hotpink" />
       </mesh>
